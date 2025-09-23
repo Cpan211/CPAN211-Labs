5f750d8fa50d33c1eb423d20b7dcfa28be93039f
Ly-Christin Mugisha <145860369+Cpan211@users.noreply.github.com>
2025-09-23 09:46:35 -0400

    Update Lab3_Main.java

--- Lab3_Main.java
+++ Lab3_Main.java
@@ -1,1 +1,33 @@
+ public class Lab3_Main {
 
+ 
+
+   public static void main(String[] args) {
+
+    // 1) Read from People.txt
+
+    // 2) Save your objects in their respective arrays
+
+    // 3) Print your arrays to console
+
+    // 4) write to files using method WriteToFile()
+
+ 
+
+   }
+
+      /** 
+
+   * Optional but recommended
+
+   */
+
+ 
+
+   //create a readFromFile() method
+
+   //create WriteToFile() method
+
+   
+
+ }
